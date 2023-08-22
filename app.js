1584b19fee6323b0c1a84e9243abb6ca1b251308
Mistyttm <emmey.leo@gmail.com>
2023-08-22 12:25:06 +1000

    initialise the application

---
+++ app.js
@@ -0,0 +1,13 @@
+// imports
+var express = require("express");
+var passport = require("passport");
+var session = require("express-session");
+var passportSteam = require("passport-steam");
+var SteamStretegy = passportSteam.Strategy;
+var app = express();
+
+var port = 3000;
+
+app.listen(port, () => {
+    console.log('Listening, port ' + port);
+})
