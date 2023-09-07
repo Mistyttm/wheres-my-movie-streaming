f2b0012a4ca9fa37d4cdddafa8f9b84654422986
Mistyttm <emmey.leo@gmail.com>
2023-09-07 23:40:03 +1000

    Rename source directory

---
+++ src/server/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express(); // create express app
+const path = require("path");
+
+// add middlewares
+app.use(express.static(path.join(__dirname, "..", "app", "dist")));
+app.use(express.static("public"));
+
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "..", "app", "dist", "index.html"));
+});
+
+// start express server on port 5000
+app.listen(5001, () => {
+    console.log("server started on port 5000");
+});
