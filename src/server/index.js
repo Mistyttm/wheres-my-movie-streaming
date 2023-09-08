87c7428bcccf64cc0653253a14c9da7f743c7845
Mistyttm <emmey.leo@gmail.com>
2023-09-08 20:48:17 +1000

    Routes

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express(); // create express app
 const path = require("path");
+const cors = require("cors");
+const logger = require("morgan");
+
+const ipRouter = require("./routes/ip");
+const moviesRouter = require("./routes/movies");
+const servicesRouter = require("./routes/services");
 
 // add middlewares
 app.use(express.static(path.join(__dirname, "..", "app", "dist")));
 app.use(express.static("public"));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(logger("dev"));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "app", "dist", "index.html"));
@@ -13,6 +23,11 @@ app.get("/", (req, res) => {
 app.get("/api/hello", (req, res) => {
     res.send({ express: "Hello From Express" });
 });
+
+// Routes
+app.use("/api/ip", ipRouter);
+app.use("/api/movies", moviesRouter);
+app.use("/api/services", servicesRouter);
 
 // start express server on port 5000
 app.listen(5000, () => {
