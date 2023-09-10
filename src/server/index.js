491e33b9e323e0063d71769862b04275edee7d14
Mistyttm <emmey.leo@gmail.com>
2023-09-10 17:37:55 +1000

    Update morgan and movie error

--- src/server/index.js
+++ src/server/index.js
@@ -14,7 +14,7 @@ app.use(express.static("public"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(logger("dev"));
+app.use(logger("combined"));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "app", "dist", "index.html"));
