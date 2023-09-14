fcf6b335fb66c86f9e751fb74ed04d37459878d6
Mistyttm <emmey.leo@gmail.com>
2023-09-14 11:26:15 +1000

    lastnights work idk wtf it was

--- src/app/src/main.tsx
+++ src/app/src/main.tsx
@@ -1,7 +1,16 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./pages/home.tsx";
+import Movie from "./pages/moviePage.tsx";
+import Tv from "./pages/tv.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/movie" element={<Movie />} />
+            <Route path="/tv" element={<Tv />} />
+        </Routes>
+    </BrowserRouter>
+);
---
+++ src/app/src/pages/home.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function App() {
+    return(
+        <div>
+            <Link to="/movie">Movie</Link>
+            <Link to="/tv">TV</Link>
+        </div>
+    )
+}
+
+export default App;
